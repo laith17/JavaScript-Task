4021b345c529762715b1e35852b4b564e43ba777
Laith007 <laithalmajaly007@gmail.com>
2023-09-29 16:46:17 +0300

    JavaScript-Task

--- script.js
+++ script.js
@@ -62,3 +62,70 @@
 // const sum = numbe1 + numbe2;
 
 // alert(`${numbe1} + ${numbe2} = ${sum}`);
+
+////////////////////////////////////////////////////////////////////////!
+
+//* Q6: If you have a “num” variable,  Your task is to print:
+
+//* - ONE, if it is equal to 1 .
+//* - TWO, if it is equal to 2.
+//* - THREE, if it is equal to 3 .
+//* - FOUR, if it is equal to 4.
+//* - FIVE, if it is equal to 5.
+//* - SIX, if it is equal to 6 .
+//* - SEVEN, if it is equal to 7.
+//* - EIGHT, if it is equal to 8.
+//* - NINE, if it is equal to 9.
+//* - PLEASE TRY AGAIN, if there is none of the above.
+
+// const num = 5;
+
+// switch (num) {
+//   case 1:
+//     if (num === 1) {
+//       console.log("ONE");
+//     }
+//     break;
+//   case 2:
+//     if (num === 2) {
+//       console.log("TWO");
+//     }
+//     break;
+//   case 3:
+//     if (num === 3) {
+//       console.log("THREE");
+//     }
+//     break;
+//   case 4:
+//     if (num === 4) {
+//       console.log("FOUR");
+//     }
+//     break;
+//   case 5:
+//     if (num === 5) {
+//       console.log("FIVE");
+//     }
+//     break;
+//   case 6:
+//     if (num === 6) {
+//       console.log("SIX");
+//     }
+//     break;
+//   case 7:
+//     if (num === 7) {
+//       console.log("SEVEN");
+//     }
+//     break;
+//   case 8:
+//     if (num === 8) {
+//       console.log("EIGHT");
+//     }
+//     break;
+//   case 9:
+//     if (num === 9) {
+//       console.log("NINE");
+//     }
+//     break;
+//   default:
+//     console.log("PLEASE TRY AGAIN");
+// }
